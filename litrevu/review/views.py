11c8c202cb6f836a5e3654c789c076bba8077bbb
Mathis Chatelain <mathis.chatelain@socotec.com>
2023-07-11 22:35:53 +0200

    working css

--- litrevu/review/views.py
+++ litrevu/review/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def hello(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+    return render(request, "review/home.html", {"name": "timéo"})
